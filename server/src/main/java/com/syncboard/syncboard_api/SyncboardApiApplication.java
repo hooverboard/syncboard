141e0d7d0970bd77c51d089de58da360745c9f38
hever boechat <heveritosouza@gmail.com>
2026-01-28 20:26:14 -0300

    first commit

---
+++ server/src/main/java/com/syncboard/syncboard_api/SyncboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.syncboard.syncboard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncboardApiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/syncboard/syncboard_api/SyncboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncboard.syncboard_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
